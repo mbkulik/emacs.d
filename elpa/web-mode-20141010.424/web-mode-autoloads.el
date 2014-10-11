0fb8b0f8b7202470269ac9ccfafcbb800ae9bf28
Michael B. Kulik <mike.b.kulik@gmail.com>
2014-10-11 15:45:22 -0400

    update org-mode and web-mode

---
+++ elpa/web-mode-20141010.424/web-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21559 57371
+;;;;;;  43358 288000))
+;;; Generated autoloads from web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21559
+;;;;;;  57371 139489 741000))
+
+;;;***
+
+(provide 'web-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-mode-autoloads.el ends here
